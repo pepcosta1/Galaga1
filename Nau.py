5991e338d71e4aa3b905a214e1770a252e0097ce
pepcosta1 <a2510726@institutmontilivi.cat>
2025-10-08 09:49:27 +0200

    he afegit un nou fitxer Nau.py

--- Nau.py
+++ Nau.py
@@ -18,15 +18,19 @@ def main():
         if op=='d':
             pass
             #sumar 1 a la variable posX
+	    posX=posX+1
         elif op=='a':
             pass
             #restar 1 a la variable posX
+	    posX=posX-1
         elif op=='w':
             pass
             #sumar 1 a la variable posY
+	    posY=posY+1
         elif op=='s':
             pass
             #restar 1 a la variable posY
+	    posY=posY-1
         elif op=='0':
             sortir=True
             print("Has sortit de la nau")
